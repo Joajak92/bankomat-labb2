8f4422033fb9410cfe2e8254a95f669042e291ad
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-02-06 11:41:06 +0100

    push kollar github actions

--- src/test/java/se/iths/joakim/bankomatlabb2/playwright/PlaywrightTest.java
+++ src/test/java/se/iths/joakim/bankomatlabb2/playwright/PlaywrightTest.java
@@ -11,8 +11,6 @@ public class PlaywrightTest {
     static Playwright playwright;
     static Browser browser;
 
-    // kommentar för att testa playwright tester i guthub actions
-
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
